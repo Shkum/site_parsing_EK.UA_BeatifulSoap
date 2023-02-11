fd43e56c8b4159bd02886aa2036882941ae38629
Error <shkum79@ukr.net>
2023-02-11 15:49:21 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+# https://www.avito.ru/moskva/telefony/mobilnye_telefony/samsung-ASgBAgICAkS0wA2crzmwwQ2I_Dc?p=1
+
+
+
+def main():
+    pass
+
+
+
+
+
+if __name__ == '__main__':
+    main()
